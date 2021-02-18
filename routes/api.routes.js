6f308f6df399c2452db9764d17364a00011e11db
Jaime <jaimeferncast@gmail.com>
2021-02-18 19:06:06 +0100

    trabajando en los comentarios

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 const User = require("../models/user.model")
 const Place = require("../models/place.model")
 
-// Búsqueda de lugar haciendo click en el mapa
+// Search place by clicking in the map
 router.post('/', (req, res, next) => {
     axios.get(`https://api.stormglass.io/v2/weather/point?lat=${req.body.lat}&lng=${req.body.lng}&params=${req.body.params}`, {
         headers: { 'Authorization': process.env.APIKEY }
@@ -18,43 +18,45 @@ router.post('/', (req, res, next) => {
         .catch(error => next(new Error(error)))
 })
 
-// Agregar lugar a favoritos
+// Add place to my places
 router.put('/', (req, res, next) => {
 
     Place
-        .create({
-            name: req.body.name,
-            coordinates: req.body.coordinates,
-            weather: req.body.weather
-        })
+        .findOne({ name: req.body.name })
         .then(response => {
-
-            const favourites = req.user.favourites
-            favourites.push(response._id)
-
-            return User.findOneAndUpdate({ _id: req.user._id }, { favourites }, { new: true })
+            if (response) {
+                const favourites = req.user.favourites
+                favourites.push(response._id)
+                return User.findOneAndUpdate({ _id: req.user._id }, { favourites }, { new: true })
+            } else {
+                return Place
+                    .create({
+                        name: req.body.name,
+                        coordinates: req.body.coordinates,
+                        weather: req.body.weather
+                    })
+                    .then(response => {
+                        const favourites = req.user.favourites
+                        favourites.push(response._id)
+                        return User.findOneAndUpdate({ _id: req.user._id }, { favourites }, { new: true })
+                    })
+            }
         })
         .then(() => res.sendStatus(203))
         .catch(error => next(new Error(error)))
 })
 
-// Búsqueda de places por nombre desde weather-user.js para sacar su info en mis-lugares
+// Place query from weather-user.js to display its weather info in my places
 router.post('/user-places', (req, res, next) => {
 
-    let promiseArr = req.body.placeName.map(elm => {
-
-        return Place
-            .findOne({ name: elm })
-            .select('weather name')
-            .then(place => { return place })
-    })
+    let promiseArr = req.body.placeName.map(elm => Place.findOne({ name: elm }).select('weather name'))
 
     Promise.all(promiseArr)
         .then(places => res.json({ places }))
         .catch(error => next(new Error(error)))
 })
 
-// Búsqueda de lugar por input
+// Search place by name with the search input
 router.get('/:location', (req, res, next) => {
 
     axios.post(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.params.location}&inputtype=textquery&fields=formatted_address,name,geometry&key=${process.env.MAPSKEY}`)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const passport = require("passport")
 
 const { checkLoggedIn } = require('./../middleware')
 
 const User = require("../models/user.model")
-const Place = require("../models/place.model")
 
+// Personal dashboard with my favourite places listed
 router.get('/mis-lugares', checkLoggedIn, (req, res, next) => {
 
     User
@@ -16,15 +15,45 @@ router.get('/mis-lugares', checkLoggedIn, (req, res, next) => {
         .catch(error => next(new Error(error)))
 })
 
-router.get('/editar', (req, res) => res.render('users/edit-profile', req))
+// Edit profile page
+router.get('/editar', checkLoggedIn, (req, res) => res.render('users/edit-profile', req))
 
-router.post('/editar', (req, res, next) => {
+// Profile changes post to DB
+router.post('/editar', checkLoggedIn, (req, res, next) => {
 
     const { username, name, email, avatar } = req.body
 
     User
         .findOneAndUpdate({ _id: req.user._id }, { username, name, email, avatar }, { new: true })
         .then(() => res.redirect('/area-personal/mis-lugares'))
+        .catch(error => next(new Error(error)))
+})
+
+// Delete a place from my places
+router.get('/eliminar-lugar/:_id', checkLoggedIn, (req, res, next) => {
+
+    const favourites = req.user.favourites
+    favourites.forEach((elm, i, array) => {
+        elm == req.params._id && array.splice(i, 1)
+    })
+
+    User
+        .findOneAndUpdate({ _id: req.user._id }, { favourites }, { new: true })
+        .populate('favourites')
+        .then(user => res.render('users/my-places', { user }))
+        .catch(error => next(new Error(error)))
+})
+
+// Add place to my places from the place page
+router.get('/agregar-lugar/:_id', checkLoggedIn, (req, res, next) => {
+
+    const favourites = req.user.favourites
+    favourites.push(req.params._id)
+
+    User
+        .findOneAndUpdate({ _id: req.user._id }, { favourites }, { new: true })
+        .populate('favourites')
+        .then(user => res.render('users/my-places', { user }))
         .catch(error => next(new Error(error)))
 })
 
